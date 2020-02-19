ea9ec3d0295293928f11186612a7b21d1850a9e1
Natanael Medina <natmet@MacBook-Pro-de-Natanael.local>
2020-02-19 17:46:43 -0400

    primer commit

---
+++ src/main/java/itla/natmet/empleosapp/EmpleosAppApplication.java
@@ -0,0 +1,15 @@
+package itla.natmet.empleosapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpleosAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpleosAppApplication.class, args);
+	}
+
+       
+       
+}
---
+++ src/test/java/itla/natmet/empleosapp/EmpleosAppApplicationTests.java
@@ -0,0 +1,13 @@
+package itla.natmet.empleosapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpleosAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
